ecf4edb4d8b5db36454c46ee1ea46901b45dc07f
Viktor <Viktor841995@gmail.com>
2023-05-15 22:46:26 +0300

    homework#7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl= document.querySelector(".gallery");
+const imgCard = createImgCard(galleryItems);
+
+galleryEl.insertAdjacentHTML ("beforeend", imgCard);
+
+galleryEl.addEventListener("click", onImgOriginalCard);
+
+function createImgCard(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+    <li class = "gallery__items">
+        <a class = "gallery__link" href = "${original}">
+            <img 
+                class = "gallery__image" 
+                src = "${preview}"
+                data-source = "${original}"
+                alt = "${description}"
+            />
+        </a>
+    </li>`
+    }).join("");
+}
+
+function onImgOriginalCard(event) {
+    event.preventDefault()
+    if(!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    console.log(event.target)
+}
